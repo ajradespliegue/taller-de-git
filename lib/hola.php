67063f39c0637169f006ad98aacd4780d9343131
antoniojra <aramarc046@g.educaand.es>
2024-10-30 13:24:09 +0100

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,5 +1,5 @@
 <?php
 // Autor: antoniojra <antoniojra99@gmail.com>
-// El nombre por defecto es Mundo
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
-@print "Hola, {$nombre}\n";
+print "Introduce tu nombre:";
+$nombre = trim(fgets(STDIN));
+@print "Hola, {$nombre}\n"
